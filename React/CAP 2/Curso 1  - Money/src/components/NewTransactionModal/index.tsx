6d468059644d8e025bbbd2ea4a29831b7f29acca
Patrick Perosa <patrickperosapp@gmail.com>
2021-05-28 01:03:11 -0300

    create form button modal

--- React/CAP 2/Curso 1  - Money/src/components/NewTransactionModal/index.tsx
+++ React/CAP 2/Curso 1  - Money/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import { Container } from './styles'
 import Modal from 'react-modal'
-
+import closeImg from '../../assets/close.svg'
 interface NewTransactionModalProps {
   isOpen: boolean
   onRequestClose: () => void
@@ -11,11 +11,31 @@ function NewTransactionModal({
   onRequestClose
 }: NewTransactionModalProps) {
   return (
-    <Container>
-      <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-        <h2>Cadastrar Transacao</h2>
-      </Modal>
-    </Container>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar Transação</h2>
+
+        <input placeholder="Titulo" />
+
+        <input type="number" placeholder="Valor" />
+
+        <input placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
   )
 }
 
